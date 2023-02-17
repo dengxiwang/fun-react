3cd41f4f8026cf24b13106b39408c556aeb9277c
dengxiwang <dengxiwang@vip.qq.com>
2023-02-17 19:46:10 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import 'nprogress/nprogress.css';
+import { React } from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import { Router, RouterBeforeEach } from './myRouters';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <RouterBeforeEach />
+    <Router />
+  </BrowserRouter>
+);
